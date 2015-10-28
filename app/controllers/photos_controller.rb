e204bded1396433487b1d1ae20f694f99df94e0c
drecinos37 <drecinos@chicagobooth.edu>
2015-10-28 00:58:35 -0500

    Part 1 updated

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,66 @@
 class PhotosController < ApplicationController
+
   def index
     @list_of_photos = Photo.all
   end
+def show
+
+@selectedphoto = Photo.find_by({:Id => params["id"].to_f})
+@list_of_photos = Photo.all
+
+render("photos.html.erb")
+end
+
+def new_form
+
+render("new_form.html.erb")
+
+end
+
+def create_row
+
+
+@createphoto = Photo.new
+@createphoto.source = params["the_source"]
+@createphoto.caption = params["the_caption"]
+@createphoto.save
+
+redirect_to("http://localhost:3000/photos")
+
+end
+
+def destroy
+
+@selectedphoto = Photo.find_by({:Id => params["id"].to_f})
+@selectedphoto.destroy
+
+redirect_to("http://localhost:3000/photos")
+
+end
+
+def edit_form
+
+@selectedphoto = Photo.find_by({:Id => params["id"].to_f})
+
+
+render("edit.html.erb")
+
+end
+
+
+def update_row
+
+
+@editphoto = Photo.find_by({:Id => params["id"].to_f})
+@editphoto.source = params["the_source"]
+@editphoto.caption = params["the_caption"]
+@editphoto.save
+
+
+redirect_to("http://localhost:3000/photos/#{@editphoto.id}")
+
+end
+
 
 
 end
